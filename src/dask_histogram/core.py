55401528e8a849d9dfc022a91f41c97fb5b79742
Doug Davis <ddavis@ddavis.io>
2021-03-29 14:08:42 -0400

    pretty functional

--- src/dask_histogram/core.py
+++ src/dask_histogram/core.py
@@ -1,27 +1,57 @@
-# stdlib
-from typing import Any, Optional
-
 # boost-histogram and dask
 import boost_histogram as bh
-import dask.array as da
-from dask.delayed import delayed, Delayed
-
-# additional third party
-import numpy as np
+from dask.delayed import delayed
 
 
-def blocked_fill(
-    data: np.ndarray, hist: bh.Histogram, weight: Optional[np.ndarray] = None
-) -> bh.Histogram:
+@delayed
+def blocked_fill_1d(data, hist, weight=None):
     hist_for_block = bh.Histogram(*hist.axes, storage=hist._storage_type())
     hist_for_block.fill(data, weight=weight)
     return hist_for_block
 
 
-def fill_1d(
-    data: da.Array, hist: bh.Histogram, weight: Optional[da.Array] = None
-) -> Delayed:
+def fill_1d(data, hist, weight=None):
     d_data = data.to_delayed()
-    d_histograms = [delayed(blocked_fill)(a, hist) for a in d_data]
+    if weight is None:
+        d_histograms = [blocked_fill_1d(a, hist) for a in d_data]
+    else:
+        d_weight = weight.to_delayed()
+        d_histograms = [blocked_fill_1d(a, hist, w) for a, w in zip(d_data, d_weight)]
+    s = delayed(sum)(d_histograms)
+    return s
+
+
+@delayed
+def blocked_fill_nd(*args, hist, weight=None):
+    hist_for_block = bh.Histogram(*hist.axes, storage=hist._storage_type())
+    hist_for_block.fill(*args, weight=weight)
+    return hist_for_block
+
+
+def fill_nd(*args, hist, weight=None):
+    ## total number of dimensions
+    D = len(args)
+    ## each entry is data along a specific dimension
+    stack_of_delayeds = [a.to_delayed() for a in args]
+    ## we assume all dimensions are chunked identically
+    npartitions = len(stack_of_delayeds[0])
+    ## we need to create a data structure that will connect coordinate
+    ## chunks we loop over the number of partitions and connect the
+    ## ith chunk along each dimension (loop over j is the loop over
+    ## the dimensions).
+    partitioned_fused_coordinates = [  # <-- list of tuples
+        tuple(stack_of_delayeds[j][i] for j in range(D)) for i in range(npartitions)
+    ]
+
+    if weight is None:
+        d_histograms = [
+            blocked_fill_nd(*d, hist=hist) for d in partitioned_fused_coordinates
+        ]
+    else:
+        d_weight = weight.to_delayed()
+        d_histograms = [
+            blocked_fill_nd(*d, hist=hist, weight=w)
+            for d, w in zip(partitioned_fused_coordinates, d_weight)
+        ]
     s = delayed(sum)(d_histograms)
     return s
--- tests/test_onedim.py
+++ tests/test_onedim.py
@@ -1,12 +1,12 @@
 import boost_histogram as bh
 import dask.array as da
-from dask_histogram.core import fill_1d
+import dask_histogram.core as dh
 
 
-def test_simple_weighted():
+def test_simple():
     h = bh.Histogram(bh.axis.Regular(12, -3.0, 3.0))
     x = da.random.standard_normal(size=(200,), chunks=50)
-    delayed_hist = fill_1d(x, h)
+    delayed_hist = dh.fill_nd(x, hist=h)
     result = delayed_hist.compute()
 
     x = x.compute()
@@ -16,14 +16,41 @@ def test_simple_weighted():
     assert h.sum() == result.sum()
 
 
-# def test_simple_weighted():
-#     h = bh.Histogram(bh.axis.Regular(12, -3.0, 3.0), storage=bh.storage.Weight())
-#     x = da.random.standard_normal(size=(200,), chunks=50)
-#     w = da.random.uniform(size=(200,), chunks=x.chunksize[0])
-#     delayed_hist = fill_1d(h, x, weight=w)
-#     result = delayed_hist.compute()
+def test_simple_weighted():
+    h = bh.Histogram(bh.axis.Regular(12, -3.0, 3.0), storage=bh.storage.Weight())
+    x = da.random.standard_normal(size=(200,), chunks=50)
+    w = da.random.uniform(0.2, 0.5, size=x.shape, chunks=x.chunksize[0])
+    delayed_hist = dh.fill_nd(x, hist=h, weight=w)
+    result = delayed_hist.compute()
 
-#     x = x.compute()
-#     w = w.compute()
-#     h = bh.Histogram(bh.axis.Regular(12, -3.0, 3.0), storage=bh.storage.Weight())
-#     h.fill(x, weight=w)
+    x = x.compute()
+    w = w.compute()
+    h = bh.Histogram(bh.axis.Regular(12, -3.0, 3.0), storage=bh.storage.Weight())
+    h.fill(x, weight=w)
+
+    assert repr(h.sum()) == repr(result.sum())
+
+
+def test_simple_nd():
+    h = bh.Histogram(
+        bh.axis.Regular(20, -3.5, 3.5),
+        bh.axis.Regular(25, -3.2, 3.2),
+        bh.axis.Regular(12, 0.4, 0.6),
+        storage=bh.storage.Weight(),
+    )
+    x = da.random.standard_normal(size=(200,), chunks=25)
+    y = da.random.standard_normal(size=(200,), chunks=25)
+    z = da.random.uniform(0.4, 0.6, size=(200,), chunks=25)
+    w = da.random.uniform(0.2, 1.1, size=(200,), chunks=25)
+
+    delayed_hist = dh.fill_nd(x, y, z, hist=h, weight=w)
+    result = delayed_hist.compute()
+
+    x = x.compute()
+    y = y.compute()
+    z = z.compute()
+    w = w.compute()
+    h = bh.Histogram(*h.axes, storage=bh.storage.Weight())
+    h.fill(x, y, z, weight=w)
+
+    assert repr(h.sum()) == repr(result.sum())
