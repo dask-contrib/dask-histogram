8095bdc7ee2a5c7a0a1d2498bfe2938a5e3a6779
Doug Davis <ddavis@ddavis.io>
2021-05-10 13:02:26 -0400

    bins fix

--- src/dask_histogram/bins.py
+++ src/dask_histogram/bins.py
@@ -61,7 +61,7 @@ def bins_style(ndim: int, bins: BinType) -> BinsStyle:
         return BinsStyle.SingleScalar
     elif isinstance(bins, (tuple, list)):
         # all integers in the tuple of list
-        if ndim == 1:
+        if ndim == 1 and np.ndim(bins) == 1:
             return BinsStyle.SingleSequence
         if all(isinstance(b, int) for b in bins):
             if len(bins) != ndim and ndim != 1:
