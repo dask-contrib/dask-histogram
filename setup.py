38b939c1e08ec4e26f6908c92d0c6466e37fe025
Doug Davis <ddavis@ddavis.io>
2022-06-21 09:27:54 -0400

    packaging updates (#16)

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
 from setuptools import setup
 
-extras_require = {
-    "test": ["pytest", "dask[dataframe]", "hist"],
-    "docs": [
-        "sphinx>=4.0.0",
-        "dask[dataframe]",
-        "dask-sphinx-theme>=2.0.0",
-        "autodocsumm",
-    ],
-}
-
-extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
-
-setup(
-    extras_require=extras_require,
-)
+setup()
