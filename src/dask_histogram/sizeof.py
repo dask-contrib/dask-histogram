8c0383eb922b3e80ce505b066ec9be27adb7a7c4
Doug Davis <ddavis@anaconda.com>
2023-01-12 12:26:12 -0600

    register dask.sizeof

---
+++ src/dask_histogram/sizeof.py
@@ -0,0 +1,17 @@
+import dask
+
+
+def register(sizeof):
+    import boost_histogram as bh
+
+    @sizeof.register(bh.Histogram)
+    def register_bh_Histogram(data):
+        return dask.sizeof.sizeof(data.view(flow=True))
+
+    @sizeof.register_lazy("hist")
+    def register_hist_Hist():
+        import hist
+
+        @sizeof.register(hist.Hist)
+        def register_hist_Hist(data):
+            return dask.sizeof.sizeof(data.view(flow=True))
