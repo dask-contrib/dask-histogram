f40b695694c93c0e6660c0c91cce2575586df1b7
Doug Davis <ddavis@anaconda.com>
2023-01-13 15:17:33 -0600

    do it lazy

--- src/dask_histogram/sizeof.py
+++ src/dask_histogram/sizeof.py
@@ -1,15 +1,16 @@
-import dask
-
-
 def register(sizeof):
-    import boost_histogram as bh
+    @sizeof.register_lazy("boost_histogram")
+    def lazy_register_boost_histogram_Histogram():
+        import boost_histogram as bh
+        import dask
 
-    @sizeof.register(bh.Histogram)
-    def register_bh_Histogram(data):
-        return dask.sizeof.sizeof(data.view(flow=True))
+        @sizeof.register(bh.Histogram)
+        def register_boost_histogram_Histogram(data):
+            return dask.sizeof.sizeof(data.view(flow=True))
 
     @sizeof.register_lazy("hist")
     def lazy_register_hist_Hist():
+        import dask
         import hist
 
         @sizeof.register(hist.Hist)
